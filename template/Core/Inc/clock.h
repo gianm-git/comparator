2c20e2413d7afdb8069636bcbc57c1dd2e7b0560
gianm-git <ezygn2@nottingham.ac.uk>
2019-11-25 12:45:42 +0000

    Initial commit

---
+++ template/Core/Inc/clock.h
@@ -0,0 +1,13 @@
+/*
+ * clock.c
+ *
+ * Configure 80MHz clock
+ */
+
+#ifndef CLOCK_H_
+#define CLOCK_H_
+
+//void Error_Handler(void);
+void Fast_Config(void);
+
+#endif /* CLOCK_H_ */
---
+++ template/Core/Src/clock.c
@@ -0,0 +1,78 @@
+/*
+ * clock.c
+ *
+ * Configure 80MHz clock
+ */
+
+#include "stm32l4xx_hal.h"
+#include "stm32l4xx.h"
+
+#include "stm32l476g_discovery.h"
+
+
+// Called in case of an error //
+/*
+void Error_Handler(void)
+{
+    BSP_LED_Init(LED_RED);
+    BSP_LED_Init(LED_GREEN);
+    BSP_LED_On(LED_RED);
+    BSP_LED_Off(LED_GREEN);
+
+    while(1);
+}
+*/
+void Fast_Config(void)
+{
+  /* oscillator and clocks configs */
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+
+  /* The voltage scaling allows optimising the power consumption when the device is
+     clocked below the maximum system frequency, to update the voltage scaling value
+     regarding system frequency refer to product datasheet.  */
+
+  /* Enable Power Control clock */
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  if(HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+  {
+    /* Initialisation Error */
+    Error_Handler();
+  }
+
+  /* Disable Power Control clock */
+  __HAL_RCC_PWR_CLK_DISABLE();
+
+  /* 80 Mhz from PLL with MSI 8Mhz as source clock */
+  /* MSI is enabled after System reset, activate PLL with MSI as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_7;   /* 8 Mhz */
+  RCC_OscInitStruct.MSICalibrationValue = RCC_MSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_MSI;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 20;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  RCC_OscInitStruct.PLL.PLLP = 7;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    /* Initialisation Error */
+    Error_Handler();
+  }
+
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
+     clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+    /* Initialisation Error */
+    Error_Handler();
+  }
+}
